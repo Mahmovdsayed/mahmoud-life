dfc9ff539e9bbdbdad190550c2c472e03239eb7e
Mahmovdsayed <mahmoudsayed3576@gmail.com>
2025-10-09 21:39:07 +0300

    next-config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  compress: true,
+  compiler: {
+    removeConsole: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+    ],
+  },
+  experimental: {
+    optimizeCss: true,
+  },
 };
 
 export default nextConfig;
